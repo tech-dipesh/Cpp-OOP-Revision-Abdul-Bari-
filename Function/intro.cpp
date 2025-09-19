ebc19eda7c4f302b7baeb5fc31a0a9416ac628c9
Dipesh Sharma <dipsharmadev@gmail.com>
2025-09-19 10:50:50 +0530

    add the function intro file

---
+++ Function/intro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void display(){
+  cout<<"Hello world for testing";
+}
+float add(float a, float b){
+  return a+b;
+}
+int main(){
+  display();
+    // int val=2942;
+    // display(val);
+  cout<<add(2.97, 2.97249);
+return 0;
+}
