c6e0438a5507f81f9ffac9ec2abf160b4a2259db
Dipesh Sharma <dipsharmadev@gmail.com>
2025-09-19 10:49:46 +0530

    write the function pointer 25/08/25

---
+++ Pointer/function-pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int max(int x, int y){
+  return x>y?x:y;
+}
+int min(int x, int y){
+  return x<y?x:y;
+}
+int main(){
+  int (*fp) (int, int);
+  fp=max;
+  (*fp)(10, 5);
+  fp=min;
+  (*fp)(10, 5);
+return 0;
+}
