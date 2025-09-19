8d3ec8b2db9f46267dd9d49a992cba460efc80e6
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-19 15:26:38 +0530

    add the intro file of the class string, september 1 2025

---
+++ String/class-string/intro.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+//     string name="dipesh";
+// cout<<name;
+
+// string val;
+// // not cin will only take the single wordw where we've to use the getline value:
+// cin>>val;
+// cout<<"Welcome to the:"<<val;
+
+// get value:
+// string cp;
+// getline(cin, cp);
+// cout<<"Welcome to the:"<<cp;
+
+
+// it's a self managed the value:
+string value;
+getline(cin, value);
+getline(cin, value);
+cout<<value;
+return 0;
+}
