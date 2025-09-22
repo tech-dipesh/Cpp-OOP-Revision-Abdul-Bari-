b73d24166b8f877212c3878c06910799c26d22c7
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-22 22:52:38 +0530

    add the rela life exmaple inheritance code

---
+++ Inheritance(25-09-02)/inheritance-real-example.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+class Student{
+  int rollno;
+  string name;
+  int mathmarks;
+  int computermarks;
+  int programmingmarks;
+  public:
+  int SumMarks(){
+    return mathmarks+computermarks+programmingmarks;
+  }
+  int Grade(){
+    return (mathmarks+computermarks+programmingmarks)/3;
+  }
+  void setRollNo(int rollno){
+    if(rollno<0) this->rollno=1;
+    else this->rollno=rollno;
+  }
+  int getRollNo(){
+    return rollno;
+  }
+  void setName(string name){
+    if(name.size()<1) this->name="Test";
+    else this->name=name;
+  }
+  void setMathmarks(int math){
+    if(math<1) this->mathmarks=1;
+    else this->mathmarks=math;
+  }
+  int getComputerMarks(){
+    return computermarks;
+  }
+  void setComputerMarks(int computer){
+    if(computer<1) this->computermarks=1;
+    else this->computermarks=computer;
+  }
+  void setProgrammMarks(int program){
+    if(program<1) this->programmingmarks=1;
+    else this->programmingmarks=program;
+  }
+
+  // getting value if needed:
+  int getRoll(){
+    return rollno;
+  }
+  string getName(){
+    return name;
+  }
+  int getMath(){
+    return mathmarks;
+  }
+  int getComputer(){
+    return computermarks;
+  }
+  int getProgram(){
+    return programmingmarks;
+  }
+
+};
+
+class University: public Student{
+  public:
+  int friendroll;
+
+  int sumRoll(){
+    return friendroll*getRollNo();
+  }
+  int computerSum(){
+    return (getComputerMarks()+getComputerMarks()+getComputerMarks())/3;
+  }
+};
+int main(){
+   Student s;
+  // setting all values:
+  s.setRollNo(2413986064);
+  // printing the output:
+  cout<<s.SumMarks()<<endl;
+  cout<<s.Grade()<<endl;
+  University u;
+  cout<<u.sumRoll()<<endl;
+  cout<<u.computerSum();
+return 0;
+}
