5977151c4d6f4d7d37b6fb3326f3d6db77b552b2
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-22 20:34:39 +0530

    add the firend operator code of teh operaor overloading

---
+++ Operator-overloading/friend-operator.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+class Complex{
+  int real;
+  int imaginary;
+  public:
+  // accessor and mutator for private value access:
+  void setReal(int real){
+    if(real<0) this->real=1;
+    else this->real=real;
+  }
+  void setImaginary(int imaginary){
+    if(imaginary<0) this->imaginary=1;
+    else this->imaginary=imaginary;
+  }
+
+  int GetReal(){
+    return real;
+  }
+  int getImaginary(){
+    return imaginary;
+  }
+
+  Complex(int real=0, int imaginary=0){
+    this->real=real;
+    this->imaginary=imaginary;
+  }
+ 
+  // cpp is the keyword to help it.
+friend  Complex operator+(Complex c1, Complex c2);
+};
+
+// don't neeed a scope resolution.
+Complex operator+(Complex c1, Complex c2)
+{
+    Complex temp;
+    temp.real=c1.real+c2.real;
+    temp.imaginary=c1.imaginary+c2.imaginary;
+    return temp;
+  }
+
+int main(){
+
+  Complex c1(14, 9);
+  Complex c2(7, 19);
+  // setting the each value:
+  c1.setReal(4);
+  c1.setImaginary(9);
+  c2.setReal(7);
+  c2.setImaginary(6);
+   Complex c3;
+  // c3=c1.add(c2);
+  c3=c1+c2;
+
+  cout<<c3.GetReal()<<"+i"<<c3.getImaginary();
+return 0;
+}
