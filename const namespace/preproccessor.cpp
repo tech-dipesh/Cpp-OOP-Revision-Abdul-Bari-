7c53631a0f13a4cbbeb1eedd9b5ea8bc169c4f3e
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-24 10:24:47 +0530

    add the preprocessor code of define the any variable with define it would replace that valeu before even compiler runs, it can be use for the fixed value to use anywhere

---
+++ const namespace/preproccessor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define PI 3.145
+#define C count
+// using namespace std;
+
+int main(){
+  // for accessing the PI value before compile start change PI to 3.145:
+  cout<<PI;
+  C<<8823;
+}
+// symbolic constant:
+// define ssqr(x) (x*x)
+// cout<<xsxr(5);
+
+// // convert strign with preproccessor:
+// 3 defien msg (g) #xor
+// cout<<msx(dip);
+
+
