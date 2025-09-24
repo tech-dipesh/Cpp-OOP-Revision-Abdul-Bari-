71718e05b91b5687d63f7e8bd95f5672452f5402
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-24 10:14:51 +0530

    fix all the error on the parent child hierarchial

--- Exception-Handling/parent-child-class-hieararchial.cpp
+++ Exception-Handling/parent-child-class-hieararchial.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
-#include <
+// #include <
 
 
 using namespace std;
 
-   class Parent{
+   class ParentException{
     public:
        const char* what() const throw() {
         return "Number has to be positive";
     }
    };
 
-   class ChildException : public Parent {
+   class ChildException : public ParentException{
 public:
     const char* what() const throw() {
         return "Child: Number has to be positive";
     }
 };
 
-class Child: public exception{
-
-  string 
+class OriginalCode{
+  public:
+  string firstThree(int myage, int abage){
+    if( myage*20>1000 && (abage*3)/5){
+        throw ParentException();
+    }
+    else{
+      return "Number are all corect order value.";
+    }
+  }
   
   
   string Set(int myage, int sagage, int abage, int dupage,int  oshage){
-    if(myage<0 || sagage<0 || abage<0 || dupage<0 || oshage<0) throw ChildException;
+    if(myage<0 || sagage<0 || abage<0 || dupage<0 || oshage<0) 
+    throw ChildException();
         else{
           return "Yes number are all correct.";
         }
@@ -32,22 +40,24 @@ class Child: public exception{
   };
 
 int main(){
+  OriginalCode o;
   int myage=20, sagage=15, abage=98, dupage=73, oshage=17;
   try{
-  cout<<Set(myage, sagage, abage, dupage, oshage)
+  cout<<o.Set(myage, sagage, abage, dupage, oshage)<<endl;
+  cout<<o.firstThree(myage, abage);
   }
   // catch(...){
   //   cout<<"it's a wrong to declare before type catch error."
   // }
   // i must alwasy return child class first.
-  catch(Child err){
-    cout<<"It's a child class: "<<err;
+  catch(ChildException& err){
+    cout<<"It's a child class: "<<err.what();
   }
-  catch(Parent err){
-    cout<<"It's a parent error: "<<err;
+  catch(ParentException& err){
+    cout<<"It's a parent error: "<<err.what();
   }
   catch(...){
-    cout<<"it's a wrong to declare before type catch error."
+    cout<<"it's a wrong to declare before type catch error.";
   }
 return 0;
 }
