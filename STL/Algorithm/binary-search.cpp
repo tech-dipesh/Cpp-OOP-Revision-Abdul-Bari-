23930af78bd5ad1f06b23473b4777d0d959cb47c
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-27 12:28:03 +0530

    add the inary search approach with params of lower and upper bound and that value, with other of first element and last element but important of binary_search

---
+++ STL/Algorithm/binary-search.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+   int binarySearch(vector<int>&vect, int val){
+      return binary_search(vect.begin(), vect.end(), val);
+    }
+    
+    //  first element will awasy be the greater than the value
+    auto lowerBound(vector<int>&vect, int val){
+     return lower_bound(vect.begin(), vect.end(), val);
+    }
+    
+    //  same like lower bound but only > not =>
+    auto upperBound(vector<int>&vect, int val){
+     return upper_bound(vect.begin(), vect.end(), val);
+    }
+    //  sum of lower and upperBound
+    auto sumUpperLower(vector<int>&vect, int val){
+     return equal_range(vect.begin(), vect.end(), val);
+   }
+
+// Only on sorted:
+int main(){
+   vector<int>li={1, 2, 5, 9, 10, 14, 18, 21, 25, 29, 30, 30, 35, 50,};
+   int value=14;
+   cout<<binarySearch(li, value)<<endl;
+   // need to dereference it.
+   auto lowbound=lowerBound(li, value);
+   cout<<*lowbound<<endl;
+   auto uppbound=upperBound(li, value);
+   cout<<*uppbound<<endl;
+
+   auto lowupval=5;
+   auto sumupplow=sumUpperLower(li, lowupval);
+   cout<<*sumupplow.second<<" "<<*sumupplow.second<<endl;
+return 0;
+}
