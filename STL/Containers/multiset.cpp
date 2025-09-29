567f95d1e53cc6c65ca4c0f3c4c913db2fb212fa
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-09-29 19:17:46 +0530

    it's a minor add on mulstiset

--- STL/Containers/multiset.cpp
+++ STL/Containers/multiset.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int main(){
-  // only collection of unqieue elements.
   multiset<int>ms;
   ms.insert(2);
   ms.insert(5);
@@ -20,10 +19,13 @@ int main(){
   for(it=ms.begin();it!=ms.end();it++){
     cout<<*it<<" ";
   }
-  // can have duplicae of sma element.
+  // can have duplicae of same element.
 
   // checking whtehr element is exist or not:
   if(ms.find(19)!=ms.end()) cout<<"yes it exist that element."<<endl;
   
+  if(ms.count(19)){
+    cout<<"19 is exist on that set element that i find from the multiset with red black tree which it internally use"
+  }
   ms.erase(8);
 }
